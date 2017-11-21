d3d2280eec2974fa9b475843d4fd587bb2dc1bb2
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2017-11-21 15:01:48 -0600

    built in 26 hours to this point. Gets info from edamam API. Can select by meat type. Save recipes to personal db. bootstrap, angular, express, mongodb.
    next step is to convert to a true angular app with views instead to see saved recipes and full recipe card instead of linking to another site.
    
    eventually build into a grocery list with the major ingredients.

---
+++ public/js/core.js
@@ -0,0 +1,111 @@
+var myApp = angular.module('myApp',[]);
+
+(function(app){
+    app.controller('recipeController',  function($scope, $http){
+        let foodTitle = document.getElementById('foodTitle');
+        let foodImage = document.getElementById('foodImg1');
+        let link = document.getElementById('foodLink');
+        let buttonId = document.getElementById('buttonId');
+        let title;
+
+        $scope.recipes = [];
+            $http({
+                method: 'GET',
+                url: "https://api.edamam.com/search?q=chicken&app_id=933b24e9&app_key=5ec51a21d4c001a6109da140caf5ecef"
+            })
+                .then (function(response){
+                    var recArr = response.data.hits;
+                    angular.forEach(recArr, function(recipeObj){
+                        $scope.recipes.push(recipeObj.recipe);
+                    });
+                    console.log($scope.recipes);
+                },  function(response){
+                    console.log('nope');
+                });
+
+        $scope.addRecipe = function($event){
+            console.log($event.recipe);
+            $http.post('/myrecipes', $event.recipe)
+                .then( (data) =>{
+                    alert('added');
+                },  (data) =>{
+                    console.log(data);
+                });
+                $scope.mySavedRecipes();
+        };
+        ($scope.recipeSearch = function(){
+            let start = Math.round((Math.random() * 10 * Math.random() * 10));
+            let end = start + 10;
+            console.log(start)
+            let protein = document.getElementById('proteinSelect').value;
+            let url = "https://api.edamam.com/search?q=" + protein +"&app_id=933b24e9&app_key=5ec51a21d4c001a6109da140caf5ecef&from=" + start + "&to=" + end;
+            console.log(url);
+            $scope.recipes = [];
+
+            $http({
+                method: 'GET',
+                url: url
+            })
+                .then (function(response){
+                    var recArr = response.data.hits;
+                    angular.forEach(recArr, function(recipeObj){
+                        $scope.recipes.push(recipeObj.recipe);
+                    });
+                    console.log($scope.recipes);
+                    angular.forEach( function(response){
+                        $scope.names.push(response.data.hits.recipe);
+                    });
+                },  function(response){
+                    console.log('nope');
+                });
+        });
+        ($scope.mySavedRecipes = function(){
+            $scope.myRecipes = [];
+            $http.get('/myrecipes')
+                .then(function(response){
+                    $scope.myRecipes = response.data;
+                    console.log($scope.myRecipes);
+                }, function(data){
+                    console.log(data);
+                });
+        })();
+        ($scope.removeSavedRecipe = ($event) =>{
+            var id = $event.recipe._id;
+
+            $http.delete( '/myrecipes/' + id)
+                .then(function(data){
+                    $scope.myRecipes = data;
+                    $scope.mySavedRecipes();
+                }),
+            function(data){
+                console.log(data);
+            };
+        });
+
+
+    });
+})(myApp);
+
+// (function getRecipes(){
+//     $.ajax({
+//         url: recipes,
+//         dataType: 'jsonp',
+//         success: function(data){
+//             title = data.hits[0].recipe.label;
+//             console.log(title);
+//             foodTitle.textContent = title;
+//             link.href = data.hits[0].recipe.url;
+//             foodImage.src = data.hits[1].recipe.image;
+//             buttonId.name = data.hits[1].recipe.uri;
+//         }
+//     });
+// })();
+
+//     $scope.getMyRecipes =function($http){
+//         $http.get('/myrecipes')
+//             .success(function(data){
+//                 foods = (data);
+//             })
+//             .error(function(data){
+//                 console.log(data);
+//             });
---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoURI = 'mongodb://admin:fakepass@ds115436.mlab.com:15436/savedrecipes';
+var methodOverride = require('method-override');
+mongoose.connection.openUri(mongoURI);
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride());
+
+var Recipe = mongoose.model('Recipe', {
+    uri: String,
+    name: String,
+    image: String,
+    url: String,
+    source: String
+});
+
+app.get('/myrecipes', (req, res)=>{
+    Recipe.find( (err, recipes) =>{
+        if (err)
+            return console.log(err);
+        res.json(recipes);
+    });
+});
+
+app.post('/myrecipes', (req, res) =>{
+    console.log(req);
+    Recipe.create({
+        'uri': req.body.uri,
+        'name': req.body.label,
+        'image': req.body.image,
+        'url': req.body.url,
+        'source': req.body.source
+    }, (err, recipe) =>{
+        if (err)
+            res.send(err);
+        Recipe.find( (err, recipes) =>{
+            if (err)
+                res.send(err);
+            res.json(recipes);
+        });
+    });
+});
+
+app.delete('/myrecipes/:id', (req, res) => {
+    Recipe.remove({
+        _id: req.params.id
+    }, function(err, recipe){
+        if (err)
+            return console.log(err);
+        Recipe.find( (err, recipes) =>{
+            if (err)
+                return console.log(err);
+            res.json(recipes);
+        });
+    });
+});
+
+app.listen(8000);
+console.log('we up on 8000 yo');
